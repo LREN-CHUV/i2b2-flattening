30174c825160b73ba2d7af6f5be261b59a40bbce
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-04-13 11:26:27 +0200

    use concept name_char

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from pandas import DataFrame
 import i2b2_connection
 
 SUBJECT_CODE_COLUMN = 'subject_code'
+DEFAULT_VOLUMES_LIST = './data/default_volumes_list'
+VOLUMES_POSTFIX = "_volume(cm3)"
 
 
 def get_volume(i2b2_conn, region, subject, dataset_prefix=''):
@@ -14,45 +16,49 @@ def get_volume(i2b2_conn, region, subject, dataset_prefix=''):
     patient_num = i2b2_conn.db_session.query(i2b2_conn.PatientMapping.patient_num).filter_by(
         patient_ide=subject).first()
     return float(i2b2_conn.db_session.query(i2b2_conn.ObservationFact.nval_num).filter_by(
-        concept_cd=region, patient_num=patient_num).first()[0])
+        name_char=region, patient_num=patient_num).first()[0])
 
 
 def main(i2b2_url, output_file, dataset_prefix='', volumes_list_path=None):
 
     logging.info("START")
 
-    volumes_list_path = volumes_list_path if volumes_list_path else './data/default_volumes_list'
+    dataset_prefix = dataset_prefix + ":" if dataset_prefix[-1] != ":" else dataset_prefix
+    volumes_list_path = volumes_list_path if volumes_list_path else DEFAULT_VOLUMES_LIST
 
+    logging.info("Reading volumes list from %s..." % volumes_list_path)
     with open(volumes_list_path, 'r') as volumes_list_file:
-        volumes_list = [v.rstrip()
-                        if len(v.rstrip()) <= 50 - len(dataset_prefix) else v.rstrip()[:47-len(dataset_prefix)] + '...'
-                        for v in volumes_list_file.readlines()]
+        volumes_list = volumes_list_file.readlines()
 
+    logging.info("Connecting to I2B2 database...")
     i2b2_conn = i2b2_connection.Connection(i2b2_url)
 
     headers = list()
     headers.append(SUBJECT_CODE_COLUMN)
 
-    for results in i2b2_conn.db_session.query(i2b2_conn.ConceptDimension.concept_cd):
-        feature = results[0][4:]
-        if feature in volumes_list:
-            headers.append(feature)
-
+    logging.info("Generating columns (one by brain region)...")
+    for vol in volumes_list:
+        concept_cd = dataset_prefix + vol.lower().replace(' ', '_') + VOLUMES_POSTFIX
+        concept_name = i2b2_conn.db_session.query(i2b2_conn.ConceptDimension.name_char).filter_by(
+            concept_cd=concept_cd).first()[0]
+        headers.append(concept_name)
     df = DataFrame(columns=headers)
 
-    subjects = list()
-    for results in i2b2_conn.db_session.query(i2b2_conn.PatientMapping.patient_ide):
-        subjects.append(results[0])
-
+    logging.info("Generating rows (one by subject)...")
+    subjects = [subj[0] for subj in i2b2_conn.db_session.query(i2b2_conn.PatientMapping.patient_ide)]
     df[SUBJECT_CODE_COLUMN] = subjects
 
+    logging.info("Filling table with data...")
     for index, row in df.iterrows():
+        subject = row[SUBJECT_CODE_COLUMN]
+        logging.info("Filling row for %s" % subject)
         for h in headers[1:]:
-            logging.info("processing %s" % row[SUBJECT_CODE_COLUMN])
-            df.loc[index, h] = get_volume(i2b2_conn, h, row[SUBJECT_CODE_COLUMN], dataset_prefix)
+            df.loc[index, h] = get_volume(i2b2_conn, h, subject, dataset_prefix)
 
     i2b2_conn.close()
+    logging.info("I2B2 database connection closed")
 
+    logging.info("Exporting data to %s CSV file..." % output_file)
     df.to_csv(output_file, index=False)
 
     logging.info("DONE")
